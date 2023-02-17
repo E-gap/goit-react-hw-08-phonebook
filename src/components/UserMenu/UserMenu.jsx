3cd13de78a1cf24b094d45c6747fdeac6ee4ff1b
E-gap <106596181+E-gap@users.noreply.github.com>
2023-02-17 23:43:47 +0200

    update userMenu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,15 +1,17 @@
 import css from './UserMenu.module.css';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../redux/auth/authOperations';
+import { useSelector } from 'react-redux';
 
 const UserMenu = () => {
+  const { name } = useSelector(state => state.auth.user);
   const dispatch = useDispatch();
   const handleClick = () => {
     dispatch(logout());
   };
   return (
     <div className={css.usermenu}>
-      <p className={css.name}>mango@mail.com</p>
+      <p className={css.name}>Welcome, {name}</p>
       <button className={css.button} type="button" onClick={handleClick}>
         Logout
       </button>
